7b9d31aa1d9a6ec9c2f21fbc2a6ae0f6fa80328c
Rinat Dobrokhotov <rinatdobr@outlook.com>
2017-09-24 18:51:43 +0300

    Add support of help file

--- help_module_handler.py
+++ help_module_handler.py
@@ -13,9 +13,36 @@ class HelpModuleHelper(KafkaWrapperModule):
         KafkaWrapperModule.__init__(self, ip, port, in_topic, out_topic, command_list)
         
     def handle_command(self, in_message):
-        text = "А ничего и нету :("
+        help_file_path = "/opt/help-module/help.txt"
+        is_help_file_exists = process_help_file(help_file_path)
+        text = "Hello from help-module (error :()"
+        if is_help_file_exists:
+            text = "To add your info edit the help file: " + help_file_path + "\n"
+            text += read_help_file(help_file_path)
         out_message = OutMessage(
             key=in_message.key,
             connector_id=in_message.connector_id,
             text=text)
         return out_message
+
+    def process_help_file(help_file_path):
+        if os.path.exists(help_file_path) == False:
+            try:
+                makedirs(path.dirname(help_file_path))
+                open(help_file_path, 'a').close()
+            except Exception as e:
+                print "Create help file error: " + help_file_path
+                print e.message
+                return False
+        return True
+
+    def read_help_file(help_file_path):
+        text = "Error :("
+        if os.path.exists(help_file_path):
+            try:
+                help_file = open(help_file_path, 'r')
+                text = help_file.read()
+            except Exception as e:
+                print "Open help file error: " + help_file_path
+                print e.message
+        return text
